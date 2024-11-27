de5c73d6e0f5d39b9ae715c9a569e11c3e7a6867
Jesús Ramis Galindo <113546651+jramisDev@users.noreply.github.com>
2024-11-27 23:48:26 +0100

    Empezamos el ejercicio

---
+++ Source/Tarea2_Bluetility/Tarea2_Bluetility.Build.cs
@@ -0,0 +1,23 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Tarea2_Bluetility : ModuleRules
+{
+	public Tarea2_Bluetility(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		if (Target.Type == TargetType.Editor)
+		{
+			//PrivateDependencyModuleNames.Add("EditorScriptingUtilities");
+			//PrivateDependencyModuleNames.Add("EditorSubsystem");
+			PrivateDependencyModuleNames.Add("UnrealEd");
+			PrivateDependencyModuleNames.Add("AssetTools");
+		}
+	}
+}
---
+++ Source/Tarea2_Bluetility/Tarea2_Bluetility.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tarea2_Bluetility.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tarea2_Bluetility, "Tarea2_Bluetility" );
